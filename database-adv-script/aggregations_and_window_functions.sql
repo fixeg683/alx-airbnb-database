ab1c13f2945a060b4f0f7bbd1e31bc1a6f169f3c
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2025-10-30 06:47:36 +0300

    Add files via upload

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,18 @@
+-- Aggregation: Total number of bookings per user
+SELECT 
+    u.id AS user_id,
+    u.name,
+    COUNT(b.id) AS total_bookings
+FROM users u
+LEFT JOIN bookings b ON u.id = b.user_id
+GROUP BY u.id, u.name;
+
+-- Window Function: Rank properties by total bookings
+SELECT 
+    p.id AS property_id,
+    p.name AS property_name,
+    COUNT(b.id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
+FROM properties p
+LEFT JOIN bookings b ON p.id = b.property_id
+GROUP BY p.id, p.name;
